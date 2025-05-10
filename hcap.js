1e8d68e24d7f7d8161b5461d3095efb93029c5f4
kyaalods <108167252+kyaalods@users.noreply.github.com>
2025-05-09 22:10:42 -0600

    Update hcap.js

--- hcap.js
+++ hcap.js
@@ -1,38 +1,16 @@
-function submitCaptchaForm() {
-  const resp = document.getElementsByName('h-captcha-response')[0].value;
-  document.getElementById('resp').value = resp;
-  window.scrollTo(0, document.body.scrollHeight);
-}
-
-function copyToClipboard() {
-  navigator.clipboard.writeText(document.getElementById('resp').value);
-  document.getElementById('cpmessage').innerHTML = '<p>Copied to clipboard!</p>';
-  setTimeout(function() {
-    document.getElementById('cpmessage').innerHTML = '';
-  }, 2500);
-}
-
 window.addEventListener('load', function () {
-    if (!navigator.clipboard) {
-      document.getElementById('copybt').style.display = 'none';
-    }
-    document.querySelector('#hcaptcha-form').onsubmit = function(e) {
-      e.preventDefault(); // avoid to execute the actual submit of the form.
-      submitCaptchaForm();
-    };
-    document.getElementById('resp').value = '';
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const siteKey = urlParams.get('sitekey');
-    if (siteKey && /^([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/.test(siteKey)) {
-      const hdiv = document.getElementById("h-captcha-div");
-      hdiv.dataset.sitekey=siteKey;
-      this.document.querySelector('#sitekey').value = siteKey;
-      hcaptcha.render('h-captcha-div', {
-        sitekey: siteKey,
-        callback: submitCaptchaForm,
-      });
-    } else {
-      this.document.querySelector('#error2').innerHTML = 'No valid sitekey provided.';
-    }
+  const hdiv = document.getElementById("h-captcha-div");
+  const siteKey = hdiv.dataset.sitekey;
+
+  if (siteKey && hcaptcha) {
+    hcaptcha.render('h-captcha-div', {
+      sitekey: siteKey,
+      callback: function (token) {
+        // Optional: auto-submit or show the token
+        console.log("h-captcha-response:", token);
+      }
+    });
+  } else {
+    document.getElementById('error2').innerText = "Failed to load hCaptcha.";
+  }
 });
